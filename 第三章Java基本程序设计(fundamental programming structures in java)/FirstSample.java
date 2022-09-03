99a502ed33cc83258e5d6f7a51352476be2cf7d2
DothyFiers <103309388+DothyFiers@users.noreply.github.com>
2022-09-03 22:49:34 +0800

    Add files via upload

---
+++ 第三章Java基本程序设计(fundamental programming structures in java)/FirstSample.java
@@ -0,0 +1,13 @@
+package chapter3;
+/*
+ * This is the first sample program in Core Java Chapter 3.
+ * @version 1.01 2022-09-3
+ * @author java loner
+ */
+public class FirstSample {
+	public static void main(String[] args) {
+		System.out.println("We will not use 'Hello, World!'.");
+		
+	}
+
+}
